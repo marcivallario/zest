2f895b480ddefc033c1f194444d1525fad325345
Marci Vallario <marcivallario@gmail.com>
2022-01-26 19:39:15 -0500

    Fixed component cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 function Card({ recipe }) {
-    console.log(recipe)
+
     return (
         <div className="card-container">
             <div className="card-img-container">
@@ -8,7 +8,9 @@ function Card({ recipe }) {
             <div className="card-details">
                 <h4>{recipe.cuisine.name}</h4>
                 <h6>Ingredients</h6>
-                {recipe.ingredients.map(ingredient => <li key={ingredient.id}>{ingredient.name}</li>)}
+                {recipe.ingredients.map(ingredient => {
+                    return (<li key={ingredient.id}>{recipe.ingredient_lists.find(list => list.ingredient_id === ingredient.id).quantity}{ingredient.name}</li>)
+                })}
                 <ol>
                     {recipe.directions.map(direction => <li key={direction.id}>{direction.text}</li>)}
                 </ol>
